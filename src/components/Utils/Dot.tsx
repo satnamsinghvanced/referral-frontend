23de5e4f69110afc74fd5d7ba3fbd7ef9e65db49
Vanced Solutions <developer@vancedsolutions.com>
2025-10-06 12:32:28 +0530

    nfc

---
+++ src/components/Utils/Dot.tsx
@@ -0,0 +1,10 @@
+
+const Dot = () => {
+    return (
+        <div className='flex justify-center items-center'>
+            <div className='bg-foreground/70 h-1 w-1 rounded-full'></div>
+        </div>
+    )
+}
+
+export default Dot
---
+++ src/components/ui/QrCode.tsx
@@ -0,0 +1,9 @@
+import { IoQrCodeOutline } from 'react-icons/io5'
+
+const QrCode = ({ className }: any) => {
+    return (
+        <IoQrCodeOutline className={className} />
+    )
+}
+
+export default QrCode
