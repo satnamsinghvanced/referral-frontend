e39d795d2b2d8253135295fc7b7e16fa61356ac5
Satnam Singh <145443805+satnamsinghvanced@users.noreply.github.com>
2026-04-08 16:49:41 +0530

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/referral-retrieve/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
